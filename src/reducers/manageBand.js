e9142bc3e107ba28fd38e6fd29ea4d44bc44b9ba
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 14:08:13 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,14 +3,12 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      const band = {
-        id: Math.random()*1000000000000,
-        name: action.name
-      }
-      return {bands: state.bands.concat(band)}
+      const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
     case 'DELETE_BAND':
-      return {bands: state.bands.filter(band => band.id !== action.id)}
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
       return state;
   }
-};
+}; 
