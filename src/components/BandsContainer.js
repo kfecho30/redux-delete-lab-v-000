9de0eded25879f34728d1affd3fd2ae5e08a8265
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 13:52:00 +0000

    automatically backed up by learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -4,6 +4,9 @@ import BandInput from './BandInput';
 import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
+  renderBands = () => {
+    return this.props.bands.map(band => <Band delete={this.props.delete} key={band.id} band={band} />)
+  }
   render() {
     return (
       <div>
