b3474bc697c283e64d3540cfb489d11823c6c743
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 13:57:47 +0000

    automatically backed up by learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -5,7 +5,7 @@ class Band extends Component {
   render() {
     return(
       <div>
-        <span>{this.props.band.bandName}</span><button onClick={() => props.delete(props.band.id)}>DELETE</button>
+        <span>{this.props.bandName}</span><button onClick={() => props.delete(props.band.id)}>DELETE</button>
       </div>
     );
   }
